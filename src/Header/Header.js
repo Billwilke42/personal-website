8f6ae10d0f0b1891cfb4160302f1ff8d919d5a28
Bill Wilke <billwilke2@gmail.com>
2020-10-31 11:36:54 -0600

    Makes header more dynamic

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,34 +5,37 @@ import { NavLink } from 'react-router-dom'
 const Header = () => {
   return (
     <header>
-        <NavLink 
-          to='/'
-          className='nav-button'
-          activeClassName='nav-button-active'
-          style={{textDecoration: 'none'}}>
-          Home
-        </NavLink>
-        <NavLink
-          to='/contact'
-          className='nav-button'
-          activeClassName='nav-button-active'
-          style={{textDecoration: 'none'}}>
-          Contact
-        </NavLink>
-        <NavLink 
-          to='/blog'
-          className='nav-button'
-          activeClassName='nav-button-active'
-          style={{textDecoration: 'none'}}>
-          Blog
-        </NavLink>
-        <NavLink 
-          to='/projects'
-          className='nav-button'
-          activeClassName='nav-button-active'
-          style={{textDecoration: 'none'}}>
-          Projects
-        </NavLink>
+      <h2 className='title-name'>Bill Wilke</h2>
+        <div className='nav-button-container'>
+          <NavLink 
+            to='/'
+            className='nav-button'
+            activeClassName='nav-button-active'
+            style={{textDecoration: 'none'}}>
+            Home
+          </NavLink>
+          <NavLink
+            to='/contact'
+            className='nav-button'
+            activeClassName='nav-button-active'
+            style={{textDecoration: 'none'}}>
+            Contact
+          </NavLink>
+          <NavLink 
+            to='/blog'
+            className='nav-button'
+            activeClassName='nav-button-active'
+            style={{textDecoration: 'none'}}>
+            Blog
+          </NavLink>
+          <NavLink 
+            to='/projects'
+            className='nav-button'
+            activeClassName='nav-button-active'
+            style={{textDecoration: 'none'}}>
+            Projects
+          </NavLink>
+        </div>
     </header>
   )
 }
