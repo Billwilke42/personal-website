2b19cdeefddfb045ec446464176dd161d4bf5554
Bill Wilke <billwilke2@gmail.com>
2020-11-04 11:32:18 -0700

    Adds basic project container

---
+++ src/ProjectContainer/ProjectContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProjectContainer.css'
+
+function ProjectContainer() {
+  return (
+    <section className='project-container'>
+      <h1>Projects coming soon!</h1>
+    </section>
+  )
+}
+
+export default ProjectContainer
